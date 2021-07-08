a0ee4061be1ac6558534b089b1af40f924b5b323
Usman <usmangill0076@gmail.com>
2021-07-08 07:54:32 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+
+  const[post,setPost]=useState([]);
+  const[id,setId]=useState(1)
+  const[loading,setLoading]=useState(true)
+   useEffect(()=>{
+     fetch("https://jsonplaceholder.typicode.com/posts/"+id)
+     .then(response=>response.json())
+     .then(data=>
+    {  
+      console.log(data);
+
+      setPost(data) ;
+       
+
+     setLoading(false)}
+     )
+
+ },[ id])
+
+ const next=_=>{
+setId(id+1) 
+ }
+
+ if(loading)
+ return <h1> Loading... </h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <  >
+    <h1>list of Posts {id}</h1>
+    <h3>
+    user id={post.userId}
+    </h3>
+    <h4>body= {post.body}</h4>
+  <button onClick={next}>next</button>
+       </>
   );
 }
 
